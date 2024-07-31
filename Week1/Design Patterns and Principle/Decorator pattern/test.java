46ae92f0e41b11a51a21a10e8629a44b88316894
SonakshiKumari <137621711+SonakshiKumari@users.noreply.github.com>
2024-07-31 23:45:56 +0530

    Create test.java

---
+++ Week1/Design Patterns and Principle/Decorator pattern/test.java
@@ -0,0 +1,14 @@
+    public static void main(String[] args) {
+        // Create an EmailNotifier instance
+        Notifier emailNotifier = new EmailNotifier();
+        
+        // Decorate with SMSNotifier
+        Notifier smsEmailNotifier = new SMSNotifierDecorator(emailNotifier);
+        
+        // Decorate further with SlackNotifier
+        Notifier slackSMSNotifier = new SlackNotifierDecorator(smsEmailNotifier);
+
+        // Send notifications through decorated notifiers
+        slackSMSNotifier.send("Hello, world!");
+    }
+}
